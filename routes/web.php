6cdd610aa1ebaa95ad2c65ca10692defd707a2f2
soodankush17 <aks21117@gmail.com>
2025-05-01 14:20:50 +0530

    chekcing route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
-    dd("her");
+Route::get('/', function () {
+    return view('welcome');
 });
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});

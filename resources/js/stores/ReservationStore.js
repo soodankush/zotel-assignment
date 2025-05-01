df586af40e76134e8a611d57976ac712fab5ef81
soodankush17 <aks21117@gmail.com>
2025-05-01 15:19:55 +0530

    url updated for api

--- resources/js/stores/ReservationStore.js
+++ resources/js/stores/ReservationStore.js
@@ -7,7 +7,7 @@ export const useReservationStore = defineStore('reservation', {
     actions: {
         async getReservationsData(from_date, till_date) {
             try{
-                const responseData = await axios.get(`http://localhost:8000/api/data/${from_date}/${till_date}`);
+                const responseData = await axios.get(`/api/data/${from_date}/${till_date}`);
                 if(responseData.status === 200) {
                     this.reservationData = responseData.data;
                     return this.reservationData;
